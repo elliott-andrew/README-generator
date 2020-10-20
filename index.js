ddc7c13f8c26cc8fdbbeb6176900d6191a2824c4
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-20 15:12:37 -0600

    Updated question array

--- index.js
+++ index.js
@@ -8,21 +8,51 @@ const { title } = require("process");
 const questions = [
     {
         type: "input",
-        name: "github",
-        message: "What is your GitHub name?"
+        name: "title",
+        message: "What is your project title?"
+
     },
     {
         type: "input",
-        name: "title",
-        message: "What is your project name?"
+        name: "description",
+        message: "How would you describe your project?"
 
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What should I know when installing this?"
+    }, {
+        type: "input",
+        name: "usage",
+        message: "Any usage examples?"
     },
     {
         type: "list",
         name: "license",
         message: "What kind of license will you use?",
-        choices: ["MIT", "None"]
-    }
+        choices: ["Apache License 2.0", "GNU General Public License v3.0", "MIT License", "BSD 2-Clause 'Simplified' License", "BSD 3-Clause 'New' or 'Revised' License", "Boost Software License 1.0", "Creative Commons Zero v1.0 Universal", "Eclipse Public License 2.0", "GNU Affero General Public License v3.0", "GNU General Public License v2.0", "GNU Lesser General Public License v2.1"]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Are there contributing notes?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "How would a user test this?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Where should people contact your for questions?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?"
+    },
 ];
 
 // function to write README file
