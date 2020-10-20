5da57c40d0aad6cc46c1f77fb56a276d5d6063f8
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-20 17:12:00 -0600

    Outlined generate markdown wth all prompts and answers

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,6 +1,24 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-    return `# ${data.title}
+function generateMarkdown(response) {
+  return `# ${response.title},
+  ## "Description",
+  ${response.description},
+  ## "Usage",
+  ${response.usage},
+  ## "Test",
+  ${response.test},
+  ## "Installation",
+  ${response.installation},
+  ## "Contributing",
+  ${response.contributing},
+  ## "License",
+  ${response.license},
+  ## "Questions",
+  ${response.questions},
+  ## "Contact"
+  ${response.email} ${response.github},
+
+  
   
   `;
 }
