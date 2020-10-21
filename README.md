3436c375be1dc5453b1b8ae44006aa8ca7aaabd8
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-21 16:04:08 -0600

    Updated readme markup

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-# README generator,
-  ## "Description",
-  Automatically generate a README file based on user input,
-  ## "Usage",
-  None,
-  ## "Test",
-  npm test,
-  ## "Installation",
-  npm i,
-  ## "Contributing",
-  No notes,
-  ## "License",
-  MIT License,
-  ## "Questions",
-  You can contact ,
-  ## "Contact"
-  andrewjc.elliott@gmail.com ,
-
-  
-  
+# README Generator
+  ## Description
+  Automatically generate a README file based on user input
+  ## Usage
+  This README file
+  ## Test
+  npm test
+  ## Installation
+  npm i
+  ## Contributing
+  Have fun
+  ## License
+  MIT License
+  ## Questions
+  Contact me on GitHub
+  ## Contact
+  andrewjc.elliott@gmail.com elliott-andrew
   
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,20 @@
 // function to generate markdown for README
 function generateMarkdown(response) {
-  return `# ${response.title},
-  ## Description,
-  ${response.description},
-  ## Usage,
-  ${response.usage},
-  ## Test,
-  ${response.test},
-  ## Installation,
-  ${response.installation},
-  ## Contributing,
-  ${response.contributing},
-  ## License,
-  ${response.license},
-  ## Questions,
-  ${response.questions},
+  return `# ${response.title}
+  ## Description
+  ${response.description}
+  ## Usage
+  ${response.usage}
+  ## Test
+  ${response.test}
+  ## Installation
+  ${response.installation}
+  ## Contributing
+  ${response.contributing}
+  ## License
+  ${response.license}
+  ## Questions
+  ${response.questions}
   ## Contact
   ${response.email} ${response.github}
   `;
